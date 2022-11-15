42278cbc354624a5064514b57183bc3064732cdb
shouvik94 <coolshouvik9@gmail.com>
2022-11-15 21:00:16 +0530

    new commit

---
+++ 8-bit-up/8-bit-up.srcs/sources_1/new/memoryBank.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 15.11.2022 20:01:31
+// Design Name: 
+// Module Name: memoryBank
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module memoryBank(
+
+    );
+endmodule
